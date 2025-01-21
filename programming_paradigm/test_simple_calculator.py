06a8dffb936db502e3c9ce86eb9caa3c8d0ad870
Yuddiee <sinclairokoro@gmail.com>
2025-01-21 18:04:59 +0100

    unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,30 +1,30 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
-class test_calc(unittest.TestCase):
+class TestSimpleCalculator(unittest.TestCase):
     #test case for the add function
 
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(10, 5), 15)
         self.assertEqual(self.calc.add(-1, 5), 4)
         self.assertEqual(self.calc.add(-10, 5), -5)
         self.assertEqual(self.calc.add(-10, -5), -15)
 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-1, 5), -6)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(10, 5), 50)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(-10, -5), 50)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(-1, 5), -0.2)
         self.assertEqual(self.calc.divide(-10, -5), 2)
